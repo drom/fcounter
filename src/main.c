407cc58870b3d491e26d09c1d903786a76e8a6bc
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2019-07-09 12:49:22 -0700

    no library version

--- src/main.c
+++ src/main.c
@@ -1,112 +1,102 @@
-#include <stddef.h>
+// frequency counter
 
-#include <metal/cpu.h>
-#include <metal/led.h>
-// #include <metal/button.h>
-#include <metal/spi.h>
-#include <metal/gpio.h>
+//
+#define GPIO_OEN     (*(volatile int *) 0x10012008)
+#define GPIO_VAL     (*(volatile int *) 0x1001200C)
+#define GPIO_IOF_EN  (*(volatile int *) 0x10012038)
+#define GPIO_IOF_SEL (*(volatile int *) 0x1001203C)
+
+#define SPI1_DIV (*(volatile int *) 0x10034000)
+#define SPI1_FMT (*(volatile int *) 0x10034040)
+#define SPI1_TXD (*(volatile int *) 0x10034048)
+#define SPI1_RXD (*(volatile int *) 0x1003404C)
+#define SPI1_TXC (*(volatile int *) 0x10034050)
+#define SPI1_IP  (*(volatile int *) 0x10034074)
 
 
-#define CASET 0x2A
-#define RASET 0x2B
-#define RAMWR 0x2C
-#define COLMOD 0x3A
-#define COLMOD16bit 0x05
+// HiFive1 Rev.B
+#define RED   0x00400000 /* 22 LED Red */
+#define GREEN 0x00080000 /* 19 LED Green */
+#define BLUE  0x00200000 /* 21 LED Blue */
+#define BLK   0x00000001 /* 0  IPS Backlight Active High */
+#define DC    0x00000002 /* 1  IPS Data/Command */
+#define RST   0x00000004 /* 2  IPS Reset Active Low */
+#define SDA1  0x00000008 /* 3  MOSI */
+#define SCL1  0x00000020 /* 5  SCK */
+#define ALL   (RED | GREEN | BLUE | BLK | DC | RST | SDA1 | SCL1)
+
+// ST7789
+#define ST77_SWRESET 0x01
+#define ST77_SLPIN   0x10
+#define ST77_SLPOUT  0x11
+#define ST77_DISPOFF 0x28
+#define ST77_DISPON  0x29
+#define ST77_CASET   0x2A
+#define ST77_RASET   0x2B
+#define ST77_RAMWR   0x2C
+#define ST77_COLMOD  0x3A
+#define ST77_RGB565  0x55
+
+void sleep(int t) {
+  while(t--) {
+    asm("nop");
+  }
+}
+
+void writeSPI1 (int val) {
+  while (SPI1_TXD); // wait for non-full
+  SPI1_TXD = val;
+  while(!(SPI1_IP & 1));
+  // sleep(100);
+}
 
 int main() {
-    struct metal_led *led0_red, *led0_green, *led0_blue;
-    led0_red = metal_led_get_rgb("LD0", "red");
-    led0_green = metal_led_get_rgb("LD0", "green");
-    led0_blue = metal_led_get_rgb("LD0", "blue");
+  GPIO_OEN = ALL;
+  SPI1_TXC = 1;
+  // HW reset
+  GPIO_VAL = ALL ^ RST ^ RED ^ GREEN;
+  sleep(2000000);
 
-    // Enable each LED
-    metal_led_enable(led0_red);
-    metal_led_enable(led0_green);
-    metal_led_enable(led0_blue);
+  // SPI1_DIV = 100; // F=?
+  // SPI1_FMT = /* Single, MSB */ 0x00080000 /* len=8 */ | 0x08/* dir=TX */ ;
+  GPIO_VAL = ALL;
+  GPIO_IOF_SEL = 0x00000000; // IOF0
+  GPIO_IOF_EN  = SCL1 | SDA1;
+  sleep(2000000);
 
-    metal_led_on(led0_red);
-    metal_led_on(led0_green);
-    metal_led_off(led0_blue);
+  // SW reset
+  GPIO_VAL = ALL ^ DC; writeSPI1(ST77_SWRESET); GPIO_VAL = ALL;
+  sleep(2000000);
 
-    struct metal_gpio *gpio;
-    gpio = metal_gpio_get_device(0);
+  // Sleep Out
+  GPIO_VAL = ALL ^ DC; writeSPI1(ST77_SLPOUT); GPIO_VAL = ALL;
+  sleep(2000000);
 
-    // backlight, 100% brightness
-    metal_gpio_enable_output(gpio, 0);
-    metal_gpio_set_pin(gpio, 0, 1);
+  // Display On
+  GPIO_VAL = ALL ^ DC; writeSPI1(ST77_DISPON); GPIO_VAL = ALL;
+  writeSPI1(ST77_RGB565);
 
-    // reset = enable
-    metal_gpio_enable_output(gpio, 2);
-    metal_gpio_set_pin(gpio, 2, 1);
+  // // column set
+  // GPIO_VAL = ALL ^ DC; writeSPI1(ST77_CASET); GPIO_VAL = ALL;
+  // writeSPI1(31 >> 8); writeSPI1(31 & 0xFF); // x0
+  // writeSPI1(63 >> 8); writeSPI1(63 & 0xFF); // x1
+  //
+  // // row set
+  // GPIO_VAL = ALL ^ DC; writeSPI1(ST77_RASET); GPIO_VAL = ALL;
+  // writeSPI1(31 >> 8); writeSPI1(31 & 0xFF); // y0
+  // writeSPI1(63 >> 8); writeSPI1(63 & 0xFF); // y1
 
-    // data / cmd
-    metal_gpio_enable_output(gpio, 1);
-    metal_gpio_set_pin(gpio, 1, 0);
+  // ram write
+  // for (int i = 0; i < 100; i++) {
+  //     GPIO_VAL = ALL ^ DC; writeSPI1(ST77_RAMWR); GPIO_VAL = ALL;
+  //     writeSPI1(0b11111111); writeSPI1(0b11111111);
+  //     //          R----G--               ---B----
+  // }
 
-    struct metal_spi *spi;
-    spi = metal_spi_get_device(1);
-
-    metal_spi_init(spi, 100000);
-
-    // Can't controll GPIOs ???
-    metal_led_off(led0_red);
-    metal_led_on(led0_green);
-    metal_led_on(led0_blue);
-
-    struct metal_spi_config config = {
-        .protocol = METAL_SPI_SINGLE,
-        .polarity = 0,
-        .phase = 0,
-        .little_endian = 0,
-        .cs_active_high = 0,
-        .csid = 0,
-    };
-
-    char tx_buf[4] = {0};
-    char rx_buf[4] = {0};
-
-    // TODO Wait?
-
-    metal_gpio_set_pin(gpio, 1, 0); // dc = low
-    metal_spi_transfer(spi, &config, 1, COLMOD, rx_buf);
-
-    metal_gpio_set_pin(gpio, 1, 1); // dc = high
-    metal_spi_transfer(spi, &config, 1, COLMOD16bit, rx_buf);
-
-    //  set window
-    //      column set
-    metal_gpio_set_pin(gpio, 1, 0); // dc = low
-    metal_spi_transfer(spi, &config, 1, CASET, rx_buf);
-
-    metal_gpio_set_pin(gpio, 1, 1); // dc = high
-    tx_buf[0] = 100 >> 8;   // x0
-    tx_buf[1] = 100 & 0xFF;
-    tx_buf[2] = 139 >> 8;   // x1
-    tx_buf[3] = 139 & 0xFF;
-    metal_spi_transfer(spi, &config, 4, tx_buf, rx_buf);
-
-    //      row set
-    metal_gpio_set_pin(gpio, 1, 0); // dc = low
-    metal_spi_transfer(spi, &config, 1, RASET, rx_buf);
-
-    metal_gpio_set_pin(gpio, 1, 1); // dc = high
-    tx_buf[0] = 100 >> 8;   // y0
-    tx_buf[1] = 100 & 0xFF;
-    tx_buf[2] = 139 >> 8;   // y1
-    tx_buf[3] = 139 & 0xFF;
-    metal_spi_transfer(spi, &config, 4, tx_buf, rx_buf);
-
-    //  send cmd
-    metal_gpio_set_pin(gpio, 1, 0); // dc = low
-    metal_spi_transfer(spi, &config, 1, RAMWR, rx_buf);
-
-    metal_gpio_set_pin(gpio, 1, 1); // dc = high
-    tx_buf[0] = 0xFF;   // color
-    tx_buf[1] = 0xFF;
-    for (int i = 0; i < (40 * 40); i++) {
-        // ram write
-        metal_spi_transfer(spi, &config, 2, tx_buf, rx_buf);
-    }
-
-    return 0;
+  while (1) {
+    sleep(2000000); GPIO_VAL = ALL ^ RED;
+    sleep(2000000); GPIO_VAL = ALL ^ GREEN;
+    sleep(2000000); GPIO_VAL = ALL ^ BLUE;
+  }
+  return 0;
 }
